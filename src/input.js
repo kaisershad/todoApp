f7cf81ba39c6bb6849deba91e5f298a499a90da9
Kaiser Anwar Shad <kaiser.anwar.shad@zalando.de>
2016-06-28 12:11:22 +0200

    chapter 1 done

--- src/input.js
+++ src/input.js
@@ -1,52 +1,53 @@
-function initInputHandlerExample1() {
-    $('#testid1').keypress(function(element){
-        if(element.which == 13){
-            alert("enter");
+function inputHandler1() {
+    $('#task1').keypress(function(element){
+        if(element.which == 13) {
+            alert(element.target.value);
         }
     });
 }
 
-function initInputHandlerExample2() {
-    $('#testid2').keypress(function(element){
-        $('#testoutput2').text(element.target.value);
+function inputHandler2() {
+    $('#task2').keypress(function(element){
+        $('#taskoutput2').text(element.target.value);
     });
 }
 
-function initInputHandlerExample3() {
-    $('#testid3').keypress(function(element){
+function inputHandler3() {
+    $('#task3').keypress(function(element){
         if(element.which == 13){
-            $('#example3list').append('<li>' +this.value+ ' </li>');
+            $('#task3list').append('<li>' +this.value+ ' </li>');
             this.value = "";
         }
     });
 }
-function initInputHandlerExample4() {
+
+function inputHandler4() {
 
     function getListEntry(value) {
-        return '<li>' + value+ ' <small class="killme">x</small></li>'
+        return '<li>' + value+ '<small class="deleteInput"> -> X</small></li>'
     }
 
-    function registerKillHandler() {
-        $('.killme').click(function(){
+    function deleteInputHandler() {
+        $('.deleteInput').click(function(){
             this.parentElement.remove();
         });
     }
 
-    $('#testid4').keypress(function(element){
+    $('#task4').keypress(function(element){
         if(element.which == 13) {
-            $('#example4list').append(getListEntry(this.value));
-            this.value = "";
-            registerKillHandler();
+            $('#task4list').append(getListEntry(this.value));
+            this.value = " ";
+            deleteInputHandler();
         }
     });
 }
 
 function init() {
     console.log("ready");
-    initInputHandlerExample1();
-    initInputHandlerExample2();
-    initInputHandlerExample3();
-    initInputHandlerExample4();
+    inputHandler1();
+    inputHandler2();
+    inputHandler3();
+    inputHandler4();
 }
 
 $(document).ready(init);
